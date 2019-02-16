286318de96bd27f2b1efac5cd3d43d78d1118cab
Dorin <d.dumitrascuta@gmail.com>
2019-02-16 21:19:04 +0000

    add disconnect event to socket

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const io = require('socket.io')(http)
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 http.listen(3000, () => console.log('listening on *: 3000'));
-io.on('connection', socket => console.log('User connected'));
+io.on('connection', socket => {
+    console.log('User connected');
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
