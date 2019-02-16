071eda007c1ed747ee4d9ab4276ecb9819abd910
Dorin <d.dumitrascuta@gmail.com>
2019-02-16 20:25:44 +0000

    intialize index.html to serve in server route

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('express')();
 const http = require('http').Server(app)
 
-app.get('/', (req, res) => res.send('<h1> Hello World <h1>'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 http.listen(3000, () => console.log('listening on *: 3000'));
